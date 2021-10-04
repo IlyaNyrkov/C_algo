03daa6928409c820e2c998ef246f0c4878ca354c
Ilya <ilya.nyrkov@gmail.com>
2021-10-04 23:49:13 +0300

    added dynamic array interface + create dynamic array func implementation

--- main.c
+++ main.c
@@ -1,33 +1,6 @@
 #include <stdio.h>
-#include <time.h>
-#include <stdlib.h>
-#include <string.h>
-
-void fill_array(int* array, int size) {
-    for (int i = 0; i < size; ++i) {
-        array[i] = i;
-    }
-}
-
-void print_array(int* array, int size) {
-    printf("{");
-    for (int i = 0; i < size; ++i) {
-        printf("%d ", array[i]);
-    }
-    printf("}");
-}
-
-void test_array_func(int* array, int size, void (*arr_func)(int*, int)) {
-    arr_func(array, size);
-    print_array(array, size);
-}
 
 int main() {
-    int n = 0;
-    scanf("%d", &n);
-    int *array = (int*)malloc(n * sizeof(int));
-    test_array_func(array, n, fill_array);
-    free(array);
-    array = NULL;
+
     return 0;
 }
