c2b694aa23730a6a22af65dc01eed680468496cc
Ilya <ilya.nyrkov@gmail.com>
2022-01-12 23:23:11 +0300

    removed free in case of assertion fail for dynamic_array test

--- dynamic_array/dynamic_array_test.cpp
+++ dynamic_array/dynamic_array_test.cpp
@@ -6,11 +6,7 @@ extern "C" {
 TEST(make_dynamic_array, empty_arr) {
     dynamic_array* array = make_dynamic_array(0);
     ASSERT_TRUE(array != NULL);
-    // Empty array if error occurs
-    ASSERT_TRUE(array->data != NULL) << [array]()->std::string {
-        free(array);
-        return "Error allocating data for dynamic_array";
-    }();
+    ASSERT_TRUE(array->data != NULL);
     free_dynamic_array(array);
 }
 
@@ -18,11 +14,7 @@ TEST(make_dynamic_array, non_empty_arr) {
     int array_capacity = 8;
     dynamic_array* array = make_dynamic_array(array_capacity);
     ASSERT_TRUE(array != NULL);
-    // if assert fails free memory allocated for dynamic_array struct
-    ASSERT_TRUE(array->data != NULL) << [array]()->std::string {
-                    free(array);
-                    return "Error allocating data for dynamic_array\n";
-                }();
+    ASSERT_TRUE(array->data != NULL);
     free_dynamic_array(array);
 }
 
@@ -32,10 +24,7 @@ TEST(push_back, preallocated_data) {
     for (int i = 0; i < array_capacity; ++i) {
         push_back(array, i);
     }
-    ASSERT_TRUE(array->data != NULL) << [array]()->std::string {
-                    free(array);
-                    return "Error allocating data for dynamic_array\n";
-                }();
+    ASSERT_TRUE(array->data != NULL);
     free_dynamic_array(array);
 }
 
@@ -45,20 +34,14 @@ TEST(push_back, resizing) {
     for (int i = 0; i < array_capacity; ++i) {
         push_back(array, i);
     }
-    ASSERT_TRUE(array->data != NULL) << [array]()->std::string {
-                    free(array);
-                    return "Error allocating data for dynamic_array\n";
-                }();
+    ASSERT_TRUE(array->data != NULL);
     free_dynamic_array(array);
 }
 
 TEST(pop_back, empty_array) {
     int array_capacity = 0;
     dynamic_array* array = make_dynamic_array(array_capacity);
-    ASSERT_TRUE(pop_back(array) == EMPTY_ARRAY_POP) << [array]()->std::string {
-                    free(array);
-                    return "Error popping back of empty array\n";
-                }();
+    ASSERT_TRUE(pop_back(array) == EMPTY_ARRAY_POP);
     free_dynamic_array(array);
 }
 
@@ -71,10 +54,6 @@ TEST(pop_back, non_empty_array) {
     for (int i = 0; i < array_capacity; ++i) {
         pop_back(array);
     }
-    ASSERT_EQ(array->size, 0) << [array]()->std::string {
-        free(array->data);
-        free(array);
-        return "Error popping back of empty array\n";
-    }();
+    ASSERT_EQ(array->size, 0);
     free_dynamic_array(array);
 }
